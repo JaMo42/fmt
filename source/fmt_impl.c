b5a0ba1f47f15f008057bad096fdc471c5bac9a7
Jakob <jakob@mohrbacher.eu>
2021-05-08 19:59:32 +0200

    fix year in copyright

--- source/fmt_impl.c
+++ source/fmt_impl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Jakob Mohrbacher
+ * Copyright (c) 2020-2021 Jakob Mohrbacher
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
